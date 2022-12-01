6cba295c3ad906e04b136b7311f6cf1101667bea
fjebaker <fergusbkr@gmail.com>
2022-12-01 09:27:23 +0000

    little refactor

--- day01/main.zig
+++ day01/main.zig
@@ -1,45 +1,48 @@
 const util = @import("../util.zig");
 const std = @import("std");
-const input = @embedFile("input.txt");
 
-pub fn main() !void {
+pub fn updateMax(max: []u32, current: u32) void {
+    if (current > max[0]) {
+        max[2] = max[1];
+        max[1] = max[0];
+        max[0] = current;
+    } else if (current > max[1]) {
+        max[2] = max[1];
+        max[1] = current;
+    } else if (current > max[2]) {
+        max[2] = current;
+    }
+}
+
+pub fn solve(input: []const u8) ![3]u32 {
     var lines = std.mem.split(u8, input, "\n");
 
-    var max : u32 = 0;
-    var max2 : u32 = 0;
-    var max3 : u32 = 0;
-    var current : u32 = 0;
+    var max: [3]u32 = .{ 0, 0, 0 };
+    var current: u32 = 0;
     while (lines.next()) |line| {
-        // std.debug.print("{s}\n", .{line});
         if (line.len == 0) {
-            // std.debug.print("-> {d}\n", .{current});
-            if (current > max) {
-                max3 = max2;
-                max2 = max;
-                max = current;
-            } else if (current > max2) {
-                max3 = max2;
-                max2 = current;
-            } else if (current > max3) {
-                max3 = current;
-            }
+            updateMax(&max, current);
             current = 0;
-            continue;
+        } else {
+            const num = try std.fmt.parseInt(u32, line, 10);
+            current += num;
         }
-        const num = try std.fmt.parseInt(u32, line, 10);
-        current += num;
     }
-    if (current > max) {
-        max3 = max2;
-        max2 = max;
-        max = current;
-    } else if (current > max2) {
-        max3 = max2;
-        max2 = current;
-    } else if (current > max3) {
-        max3 = current;
-    }
+    // make sure we get the last elf
+    updateMax(&max, current);
+    return max;
+}
 
-    std.debug.print("Part 1: {d}\n", .{max});
-    std.debug.print("Part 2: {d} + {d} + {d} = {d}\n", .{max, max2, max3, max + max2 + max3});
+pub fn main() !void {
+    const max = try solve(@embedFile("input.txt"));
+    const total = @reduce(.Add, @as(@Vector(3, u32), max));
+    std.debug.print("Part 1: {d}\n", .{max[0]});
+    std.debug.print("Part 2: {any} = {d}\n", .{ max, total });
+}
+
+test "test-input" {
+    const max = try solve(@embedFile("test.txt"));
+    const total = @reduce(.Add, @as(@Vector(3, u32), max));
+    try std.testing.expectEqual(max[0], 24000);
+    try std.testing.expectEqual(total, 45000);
 }
