1ace44be7633ecccb56d2688cde0a75e68a7c2ad
fjebaker <fergusbkr@gmail.com>
2022-12-08 18:55:44 +0000

    use allocator to avoid comptime trickery

--- day08/main.zig
+++ day08/main.zig
@@ -2,63 +2,76 @@ const std = @import("std");
 const util = @import("../util.zig");
 const AtProto = fn (usize, usize) usize;
 
+const VisMask = std.bit_set.DynamicBitSet;
 const Direction = enum { left, right, up, down };
 
-pub fn Forrest(comptime size: comptime_int) type {
-    return struct {
-        const Self = @This();
-        input: []const [size + 1]u8,
-        visible: *std.bit_set.ArrayBitSet(usize, size * size),
+const Forest = struct {
+    const Self = @This();
+    input: [][]const u8,
+    size: usize,
+    visible: *VisMask,
 
-        fn nextInbounds(_: *const Self, x: usize, y: usize) bool {
-            return x > 0 and y > 0 and y < size - 1 and x < size - 1;
-        }
+    fn nextInbounds(self: *const Self, x: usize, y: usize) bool {
+        return x > 0 and y > 0 and y < self.size - 1 and x < self.size - 1;
+    }
 
-        pub fn scoreVisible(self: *Self, x: usize, y: usize, direction: Direction) u64 {
-            var score: u64 = 1;
-            const tree = self.input[y][x];
-            var i: usize = x;
-            var j: usize = y;
-            var k: u64 = 0;
-            while (true) {
-                switch (direction) {
-                    .left => i -= 1,
-                    .right => i += 1,
-                    .up => j -= 1,
-                    .down => j += 1,
-                }
-                const neighbour = self.input[j][i];
-                if (tree <= neighbour) break;
-                if (!self.nextInbounds(i, j)) {
-                    self.visible.set(x + y * size);
-                    break;
-                }
-                k += 1;
+    pub fn scoreVisible(self: *Self, x: usize, y: usize, comptime direction: Direction) u64 {
+        var score: u64 = 1;
+        const tree = self.input[y][x];
+        var i: usize = x;
+        var j: usize = y;
+        var k: u64 = 0;
+        while (true) {
+            switch (direction) {
+                .left => i -= 1,
+                .right => i += 1,
+                .up => j -= 1,
+                .down => j += 1,
             }
-            score *= k + 1;
-            return score;
+            const neighbour = self.input[j][i];
+            if (tree <= neighbour) break;
+            if (!self.nextInbounds(i, j)) {
+                self.visible.set(x + y * self.size);
+                break;
+            }
+            k += 1;
         }
-    };
+        score *= k + 1;
+        return score;
+    }
+};
+
+fn unpack(alloc: std.mem.Allocator, input: [] const u8) ![][]const u8 {
+    var acc = std.ArrayList([]const u8).init(alloc);
+    errdefer acc.deinit();
+    var lines = std.mem.tokenize(u8, input, "\n");
+    while (lines.next()) |line| {
+        try acc.append(line);
+    }
+    return acc.toOwnedSlice();
 }
 
-fn solve(comptime input: []const u8) [2]u64 {
-    const size = comptime std.mem.indexOf(u8, input, "\n").?;
-    std.debug.print("SIZE {d}\n", .{size});
-    const VisMask = std.bit_set.ArrayBitSet(usize, size * size);
-    var visible = VisMask.initEmpty();
+fn solve(alloc: std.mem.Allocator, input: []const u8) ![2]u64 {
+    const size = std.mem.indexOf(u8, input, "\n").?;
 
-    const matrix_input = std.mem.bytesAsSlice([size + 1]u8, input);
-    var forrest: Forrest(size) = .{ .input = matrix_input, .visible = &visible };
+    var visible = try VisMask.initEmpty(alloc, size * size);
+    defer visible.deinit();
+
+    var matrix_input = try unpack(alloc, input);
+    defer alloc.free(matrix_input);
+
+    var Forest: Forest = .{ .input = matrix_input, .size = size, .visible = &visible };
+
+    var part2: u64 = 0;
 
     var x: usize = 1;
-    var part2: u64 = 0;
     const dim = size - 1;
     while (x < dim) : (x += 1) {
         var y: usize = 1;
         while (y < dim) : (y += 1) {
             var total: u64 = 1;
             inline for ([_]Direction{ .up, .down, .left, .right }) |dir| {
-                const score = forrest.scoreVisible(x, y, dir);
+                const score = Forest.scoreVisible(x, y, dir);
                 total *= score;
             }
             part2 = @maximum(total, part2);
@@ -66,20 +79,25 @@ fn solve(comptime input: []const u8) [2]u64 {
     }
 
     const part1 = visible.count() + (4 * size) - 4;
-
     return .{ part1, part2 };
 }
 
 pub fn main() !void {
-    const sol = solve(@embedFile("input.txt"));
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // var allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var allocator = arena.allocator();
+
+    const sol = try solve(allocator, @embedFile("input.txt"));
     std.debug.print("Part 1: {d}\nPart 2: {d}\n", .{ sol[0], sol[1] });
 
-    // var result = try util.benchmark(alloc, solve, .{@embedFile("input.txt")}, .{});
-    // defer result.deinit();
-    // result.printSummary();
+    var result = try util.benchmark(allocator, solve, .{allocator, @embedFile("input.txt")}, .{});
+    defer result.deinit();
+    result.printSummary();
 }
 
 test "test-input" {
-    const sol = solve(@embedFile("test.txt"));
+    const sol = try solve(std.testing.allocator, @embedFile("test.txt"));
     std.debug.print("Part 1: {d}\nPart 2: {d}\n", .{ sol[0], sol[1] });
 }
