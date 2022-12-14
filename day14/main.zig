42ab05bea38026ce3ed40724ad86fb254765a403
fjebaker <fergusbkr@gmail.com>
2022-12-14 18:47:49 +0000

    perf with new grid

--- day14/main.zig
+++ day14/main.zig
@@ -1,159 +1,148 @@
 const std = @import("std");
+const util = @import("../util.zig");
 
-const Grid = struct {
+const CoordNew = util.Coord;
+
+fn coordFromText(text: []const u8) !CoordNew {
+    const i = std.mem.indexOf(u8, text, ",").?;
+    const x = try std.fmt.parseInt(usize, text[0..i], 10);
+    const y = try std.fmt.parseInt(usize, text[i + 1 ..], 10);
+    return .{ x, y };
+}
+
+const GridNew = struct {
     const Self = @This();
     const EMPTY = ' ';
     const BLOCKED = '#';
     const SAND = 'o';
+    const GridType = util.GridT(u8);
 
-    alloc: std.mem.Allocator,
-    grid: []u8,
-    x_size: usize,
-    y_size: usize,
-    xoffset: usize,
-    last_y_block: usize = 0,
-    floor: bool = false,
+    grid: GridType,
+    x_offset: usize,
+    last_y: usize = 0,
+    block_count: u32 = 0,
 
-    pub fn init(alloc: std.mem.Allocator, x_size: usize, y_size: usize, x_mid: usize) !Grid {
-        var grid = try alloc.alloc(u8, x_size * y_size);
-        for (grid) |*item| item.* = EMPTY;
-        return .{
-            .grid = grid,
-            .x_size = x_size,
-            .y_size = y_size,
-            .xoffset = x_mid - @divFloor(x_size, 2),
-            .alloc = alloc,
-        };
-    }
-
-    const Coord = struct {
-        x: usize,
-        y: usize,
-        pub fn fromText(text: []const u8) !Coord {
-            const i = std.mem.indexOf(u8, text, ",").?;
-            const x = try std.fmt.parseInt(usize, text[0..i], 10);
-            const y = try std.fmt.parseInt(usize, text[i + 1 ..], 10);
-            return .{ .x = x, .y = y };
-        }
-        pub fn vectorTo(self: *const Coord, other: Coord) Delta {
-            const dx: i32 = @intCast(i32, other.x) - @intCast(i32, self.x);
-            const dy: i32 = @intCast(i32, other.y) - @intCast(i32, self.y);
-            // clamp within -1 and 1
-            const dx_norm = @intCast(i8, std.math.clamp(dx, -1, 1));
-            const dy_norm = @intCast(i8, std.math.clamp(dy, -1, 1));
-            return .{ .dx = dx_norm, .dy = dy_norm };
-        }
-    };
-
-    pub fn addBlock(self: *Self, line: []const u8) !void {
-        var coord_list = std.ArrayList(Coord).init(self.alloc);
-        defer coord_list.deinit();
-        var text = std.mem.tokenize(u8, line, " -> ");
-        while (text.next()) |t| try coord_list.append(try Coord.fromText(t));
-
-        const coords = coord_list.items;
-        for (coords) |*coord, i| {
-            // stop at last item
-            if (i == coords.len - 1) break;
-            const next = coords[i + 1];
-            const delta = coord.vectorTo(next);
-            while (coord.x != next.x or coord.y != next.y) {
-                self.grid[self.at(coord.x, coord.y)] = BLOCKED;
-                coord.x = @intCast(usize, @intCast(i32, coord.x) + delta.dx);
-                coord.y = @intCast(usize, @intCast(i32, coord.y) + delta.dy);
-            }
-            self.grid[self.at(next.x, next.y)] = BLOCKED;
-            // update track of where void is
-            if (coord.y > self.last_y_block) self.last_y_block = coord.y;
-            if (next.y > self.last_y_block) self.last_y_block = next.y;
-        }
-    }
-
-    pub fn at(self: *Self, x: usize, y: usize) usize {
-        const x_adjusted = x - self.xoffset;
-        if (x_adjusted >= 0 and x_adjusted < self.x_size and y >= 0 and y < self.y_size) {
-            return x_adjusted + y * self.x_size;
-        } else @panic("Out of bounds!");
+    pub fn init(
+        alloc: std.mem.Allocator,
+        x_size: usize,
+        y_size: usize,
+        x_mid: usize,
+    ) !Self {
+        var grid = try GridType.initWithValue(alloc, x_size, y_size, EMPTY);
+        return .{ .grid = grid, .x_offset = x_mid - @divFloor(x_size, 2) };
     }
 
     pub fn deinit(self: *Self) void {
-        self.alloc.free(self.grid);
+        self.grid.deinit();
     }
 
-    const Delta = struct { dx: i8, dy: i8 };
-    fn nextStep(self: *Self, x: usize, y: usize) ?Delta {
-        if (self.reachedVoid(y)) return null;
+    // translate x coordinate
+    fn xlt(self: *const Self, x: usize) usize {
+        return x - self.x_offset;
+    }
 
-        if (self.grid[self.at(x, y + 1)] == EMPTY) {
-            return .{ .dx = 0, .dy = 1 };
+    fn _fillWallGaps(self: *Self, x: usize, y: usize, v: *u8) bool {
+        const mid = v.*;
+        const left = self.grid.get(x - 1, y);
+        const right = self.grid.get(x + 1, y);
+        if (mid == BLOCKED) {
+            self.block_count += 1;
+            if (left == mid and right == mid) {
+                self.grid.set(x, y + 1, BLOCKED);
+            }
         }
-        if (self.grid[self.at(x - 1, y + 1)] == EMPTY) {
-            return .{ .dx = -1, .dy = 1 };
+        return y < self.last_y + 2;
+    }
+
+    pub fn fillWallGaps(self: *Self) void {
+        self.grid.forEachPadded(_fillWallGaps, self, 1, 0);
+    }
+
+    pub fn addBlock(self: *Self, line: []const u8) !void {
+        var coord_list = std.ArrayList(CoordNew).init(self.grid.alloc);
+        defer coord_list.deinit();
+        // parse input line
+        var text = std.mem.tokenize(u8, line, " -> ");
+        while (text.next()) |t| {
+            try coord_list.append(try coordFromText(t));
         }
-        if (self.grid[self.at(x + 1, y + 1)] == EMPTY) {
-            return .{ .dx = 1, .dy = 1 };
+
+        const coords = coord_list.items;
+        for (coords) |*p_coord, i| {
+            var coord = p_coord.*;
+            // stop at last item
+            if (i == coords.len - 1) break;
+            const next = coords[i + 1];
+            const delta = util.vecTo(coord, next);
+            while (@reduce(.Or, coord != next)) {
+                self.grid.set(
+                    self.xlt(util.getX(coord)),
+                    util.getY(coord),
+                    BLOCKED,
+                );
+                coord = util.addUnit(coord, delta);
+            }
+            self.grid.set(
+                self.xlt(util.getX(coord)),
+                util.getY(coord),
+                BLOCKED,
+            );
+            const c_y = util.getY(coord);
+            if (c_y > self.last_y) self.last_y = c_y;
+            const n_y = util.getY(next);
+            if (n_y > self.last_y) self.last_y = n_y;
+        }
+    }
+
+    fn reachedVoid(self: *const Self, y: usize) bool {
+        return y > self.last_y;
+    }
+
+    fn nextStep(self: *Self, c: CoordNew) ?util.UnitVec {
+        const x = util.getX(c);
+        const y = util.getY(c);
+        if (self.reachedVoid(y)) return null;
+        if (self.grid.get(x, y + 1) == EMPTY) {
+            return .{ 0, 1 };
+        }
+        if (self.grid.get(x - 1, y + 1) == EMPTY) {
+            return .{ -1, 1 };
+        }
+        if (self.grid.get(x + 1, y + 1) == EMPTY) {
+            return .{ 1, 1 };
         }
         return null;
     }
 
     pub fn dropSand(self: *Self, x_init: usize) bool {
-        var x: usize = x_init;
-        var y: usize = 0;
-        while (self.nextStep(x, y)) |delta| {
-            x = @intCast(usize, @intCast(i32, x) + delta.dx);
-            y = @intCast(usize, @intCast(i32, y) + delta.dy);
+        var coord = CoordNew{ self.xlt(x_init), 0 };
+        while (self.nextStep(coord)) |step| {
+            coord = util.addUnit(coord, step);
         }
-        if (self.floor) {
-            // top of sand
-            if (y == 0) return true;
-        } else {
-            if (self.reachedVoid(y)) return true;
-        }
-        self.grid[self.at(x, y)] = SAND;
+        const y = util.getY(coord);
+        if (y == 0 or self.reachedVoid(y)) return true;
+        const x = util.getX(coord);
+        self.grid.set(x, y, SAND);
         return false;
-    }
-
-    fn reachedVoid(self: *const Self, y: usize) bool {
-        return y > self.last_y_block;
-    }
-
-    pub fn prettyPrint(self: *Self) void {
-        var y: usize = 0;
-        while (y < self.y_size) : (y += 1) {
-            var x: usize = self.xoffset;
-            while (x < self.x_size + self.xoffset) : (x += 1) {
-                std.debug.print("{c}", .{self.grid[self.at(x, y)]});
-            }
-            std.debug.print("\n", .{});
-        }
-    }
-
-    pub fn reset(self: *Self) void {
-        for (self.grid) |*item| {
-            if (item.* == SAND) item.* = EMPTY;
-        }
     }
 };
 
 pub fn solve(alloc: std.mem.Allocator, input: []const u8) ![2]u32 {
-    var grid = try Grid.init(alloc, 1000, 1000, 500);
-    defer grid.deinit();
+    var tmp = try GridNew.init(alloc, 1000, 1000, 500);
+    defer tmp.deinit();
+
     var lines = std.mem.tokenize(u8, input, "\n");
-    while (lines.next()) |line| try grid.addBlock(line);
+    while (lines.next()) |line| {
+        try tmp.addBlock(line);
+    }
 
     var part1: u32 = 0;
-    while (!grid.dropSand(500)) {
+    while (!tmp.dropSand(500)) {
         part1 += 1;
     }
-    grid.reset();
-    grid.floor = true;
 
-    // add 1 for the top of the pyramid
-    var part2: u32 = 1;
-    while (!grid.dropSand(500)) {
-        part2 += 1;
-    }
-
+    tmp.fillWallGaps();
+    const part2 = @intCast(u32, std.math.pow(usize, (tmp.last_y + 2), 2)) - tmp.block_count;
     return .{ part1, part2 };
 }
 
@@ -164,9 +153,9 @@ pub fn main() !void {
     const sol = try solve(allocator, @embedFile("input.txt"));
     std.debug.print("Part 1: {d}\nPart 2: {d}\n", .{ sol[0], sol[1] });
 
-    // var result = try util.benchmark(allocator, solve, .{ allocator, @embedFile("input.txt") }, .{});
-    // defer result.deinit();
-    // result.printSummary();
+    var result = try util.benchmark(allocator, solve, .{ allocator, @embedFile("input.txt") }, .{});
+    defer result.deinit();
+    result.printSummary();
 }
 
 test "test-input" {
